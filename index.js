fbec1f9d6cb3b0f41c7e506f76b148b0693b25f9
Cory Sabol <sible777@gmail.com>
2015-12-18 22:24:03 -0500

    Verify webid cert

--- index.js
+++ index.js
@@ -4,9 +4,12 @@
 // "ldnode": "^0.2.14",
 // var ldnode = require('ldnode');
 var express = require('express');
+var session = require('express-session');
 var fs = require('fs');
 var http = require('http');
 var https = require('https');
+var webid = require('webid/tls');
+var biometric = require('./biometric');
 
 // These need to be relative
 var KEY_PATH = 'keys/key.pem';
@@ -17,6 +20,7 @@ var PORT = 3000;
 var PRIVATE_KEY = fs.readFileSync(KEY_PATH);
 var CERT = fs.readFileSync(CERT_PATH);
 
+// credentials/options for the https server
 var credentials = {
     requestCert: true,
     key: PRIVATE_KEY,
@@ -26,12 +30,37 @@ var credentials = {
 // Create the express app
 var app = express();
 
+// Use the session middleware
+app.use(session({
+    secret: 'keyboard cat',
+    cookie: { maxAge: 60000 },
+    resave: false,
+    saveUninitialize: false
+}));
+
 // Do the app routing here
 app.get('/', function (req, res) {
-    console.log('GET request /');
     var cert = req.connection.getPeerCertificate();
+
+    if (!cert) {
+        res.send('No cert provided!');
+    }
+
+    // validate the WebID CERT
+    webid.verify(cert, function (err, result) {
+        if (err) {
+            // handle the error
+            console.log('Error processing certificat: ' + err.message);
+            res.send()
+        }
+        // Store some stuff in the session
+        console.log('Webid verification result: ' + result);
+        // Store the verification result in the session
+        req.session.user_webid = result;
+        res.send('WebID verified');
+    });
+
     console.log(cert);
-    res.send('GET request /');
 })
 
 app.get(ROOT_PATH, function (req, res) {
