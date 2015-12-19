4a27a7113e597e35b33f580d6baa9540ce23e7aa
Cory Sabol <sible777@gmail.com>
2015-12-19 02:16:04 -0500

    add lib folder

--- index.js
+++ index.js
@@ -9,13 +9,14 @@ var fs = require('fs');
 var http = require('http');
 var https = require('https');
 var webid = require('webid/tls');
-var biometric = require('./biometric');
+var biometric = require('./lib/biometric');
+var validate = require('./lib/validate');
 
 // These need to be relative
 var KEY_PATH = 'keys/key.pem';
 var CERT_PATH = 'keys/cert.pem';
 var ROOT_PATH = '/ldresources';
-var PORT = 3000;
+var HTTPS_PORT = 5000;
 
 var PRIVATE_KEY = fs.readFileSync(KEY_PATH);
 var CERT = fs.readFileSync(CERT_PATH);
@@ -23,6 +24,7 @@ var CERT = fs.readFileSync(CERT_PATH);
 // credentials/options for the https server
 var credentials = {
     requestCert: true,
+    rejectUnauthorized: false,
     key: PRIVATE_KEY,
     cert: CERT
 };
@@ -39,7 +41,11 @@ app.use(session({
 }));
 
 // Do the app routing here
-app.get('/', function (req, res) {
+app.get('/', function (req, res, next) {
+    res.send('<a href="/validate">Validate WebID</a>')
+});
+
+app.get('/validate', function (req, res, next) {
     var cert = req.connection.getPeerCertificate();
 
     if (!cert) {
@@ -57,7 +63,7 @@ app.get('/', function (req, res) {
         // Store the verification result in the session
         req.session.user_webid = result;
         req.session.verified = true;
-        res.next();
+        res.send('WebID successfully validated');
     });
 });
 
@@ -66,10 +72,18 @@ app.get(ROOT_PATH, function (req, res) {
     res.send('GET request /ldresources');
 });
 
+// Create a regular HTTP server
+// var http_server = http.createServer(app)
+//     .listen(HTTP_PORT, function (req, res) {
+//     });
+
 // Set the app to listen on port 3000
 // The express app acts as middleware for the native https server
 // HTTPS!
-var https_server = https.createServer(credentials, app);
-https_server.listen(PORT, function(req, res) {
+var https_server = https.createServer(credentials, app)
+    .listen(HTTPS_PORT, function (req, res) {});
 
-});
+// var https_server2 = https.createServer({
+//     key: PRIVATE_KEY,
+//     cert: CERT
+// }, app).listen(3000, function (req, res) {});
