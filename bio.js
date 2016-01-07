7c4221325845c1be94e71b100577e1fd7801cbda
mayanicole <mayanicolee@gmail.com>
2016-01-07 10:42:09 -0500

    bio.js

---
+++ bio.js
@@ -0,0 +1,12 @@
+var path = require('path');
+var fs = require('fs');
+var acl = require('acl');
+var file_path = process.argv[1];
+var file = process.argv[2];
+
+fs.readFile(file_path, function(err, contents){
+	var file_content = contents.toString();
+}
+
+var biometric.prototype.reason = function(file_content){
+}
